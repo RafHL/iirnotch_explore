4e82fe233e091c4bc6c288f1810f74918315f017
Rafael Hernandez-Lopez <rhernandezlopez1@ufl.edu>
2024-03-07 12:45:12 -0500

    Updated batch for filtered file names

--- noise/plot_filter_batch.sh
+++ noise/plot_filter_batch.sh
@@ -7,9 +7,11 @@ mv input_csvs/*.png -t input_plots
 # Apply the filter to the input csvs
 python ../filter_notch.py input_csvs/*
 mkdir -p filtered_csvs
-mv input_csvs/filtered* -t filtered_csvs
+mv input_csvs/*filtered* -t filtered_csvs
+
 
 # Plot the filtered csvs
 python ../plot_noise.py filtered_csvs/*
 mkdir -p filtered_plots
 mv filtered_csvs/*.png -t filtered_plots
+
